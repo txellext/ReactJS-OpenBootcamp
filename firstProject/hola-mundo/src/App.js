3e46d5181542d6b2bb80c3e0dc8bd31400df0f5b
Txell Amigó <txellmgsol@gmail.com>
2023-02-01 16:50:50 +0100

    First Component, Greetings, added

--- firstProject/hola-mundo/src/App.js
+++ firstProject/hola-mundo/src/App.js
@@ -1,14 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import Greetings from './components/pure/greetings';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          HELLO WORLD!
-        </p>
+          <Greetings></Greetings>
 
       </header>
     </div>
---
+++ firstProject/hola-mundo/src/components/pure/greetings.jsx
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+
+class Greetings extends Component {
+    render() {
+        return (
+            <div>
+                <h1>
+                    HOLA
+                </h1>
+            </div>
+        );
+    }
+}
+
+
+Greetings.propTypes = {
+
+};
+
+
+export default Greetings;
