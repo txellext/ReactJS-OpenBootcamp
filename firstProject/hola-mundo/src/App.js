b055c5f16ae740a413a30c2262bca6df165ddb94
Txell Amigó <txellmgsol@gmail.com>
2023-02-01 18:20:38 +0100

    Constructor, Props, state, setState implemented

--- firstProject/hola-mundo/src/App.js
+++ firstProject/hola-mundo/src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-          <Greetings></Greetings>
+          <Greetings name="Txell"></Greetings>
 
       </header>
     </div>
--- firstProject/hola-mundo/src/components/pure/greetings.jsx
+++ firstProject/hola-mundo/src/components/pure/greetings.jsx
@@ -3,20 +3,40 @@ import PropTypes from 'prop-types';
 
 
 class Greetings extends Component {
+
+    constructor(props){
+        super(props);
+        this.state = {
+            age: 29
+        }
+    }
+
     render() {
         return (
             <div>
                 <h1>
-                    HOLA
+                    HOLA { this.props.name}
                 </h1>
+                <h2>
+                    Tu edad es de: { this.state.age}
+                </h2>
             </div>
         );
+    }
+
+    birthday = () => {
+        this.setState((prevState) => (
+            {
+                age: prevState.age + 1
+            }
+        )
+        )
     }
 }
 
 
 Greetings.propTypes = {
-
+    name: PropTypes.string,
 };
 
 
