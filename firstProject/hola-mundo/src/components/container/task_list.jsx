8c7787997a995aea3d3aeabf91c9762a8cbd3166
Txell Amigó <txellmgsol@gmail.com>
2023-02-18 11:20:12 +0100

    lifecycle control added to TaskListComponent

--- firstProject/hola-mundo/src/components/container/task_list.jsx
+++ firstProject/hola-mundo/src/components/container/task_list.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Task } from '../../models/task.class'
 import { LEVELS } from '../../models/levels.enum';
 import TaskComponent from '../pure/task';
@@ -6,9 +6,22 @@ import TaskComponent from '../pure/task';
 
 const TaskListComponent = () => {
 
+    //Let's define an initial state
     const defaultTask = new Task('Example', 'Default', false, LEVELS.NORMAL);
 
-    const changeState = () => {
+    // Component state
+    const [tasks, setTasks] = useState(defaultTask);
+
+    // Lifecycle Component Control
+    useEffect(() => {
+        console.log('Task State modified');
+
+        return () => {
+            console.log('TaskList component will unmount...')
+        };
+    }, [tasks]);
+
+    const changeCompleted = () => {
         console.log('TODO: change state task')
     }
 
