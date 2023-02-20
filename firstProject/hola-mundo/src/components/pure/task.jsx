0b7c1c9f986496a0f6f6351e7ee134dd2160e069
Txell Amigó <txellmgsol@gmail.com>
2023-02-20 13:22:09 +0100

    trash icon added

--- firstProject/hola-mundo/src/components/pure/task.jsx
+++ firstProject/hola-mundo/src/components/pure/task.jsx
@@ -31,37 +31,14 @@ const TaskComponent = ({ task }) => {
             </td>
             
             <td className='align-middle'>
-                {/* TODO: substitute for Icons  */} 
-                {/**With if */}
                 { task.completed ?
                     (<i className='bi-toggle-on' style={ {color: 'green'} }></i>) 
                     :
                     (<i className='bi-toggle-on' style={ {color: 'grey'} }></i>) 
-                
                 }
-
-
-
-                <span> {task.completed ? 'Completed' : 'Pending'} </span>
+                <i className='bi-trash' style={ {color: 'tomato'} }></i>
             </td>
         </tr>
-        
-        
-        
-        
-        // <div>
-        //     <h2 className='task-name'>
-        //         Nombre: { task.name }
-        //     </h2>
-        //     <h3>
-        //         Description: { task.description }
-        //     </h3>
-        //     <h4> Level: { task.level }
-        //     </h4>
-        //     <h5>
-        //         This task is: { task.completed ? 'COMPLETED':'PENDING'}
-        //     </h5>
-        // </div>
     );
 };
 
