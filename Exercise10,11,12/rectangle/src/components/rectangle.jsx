a1f66dcb600e1987f0ac461f4883b7649a72c8b6
Txell Amigó <txellmgsol@gmail.com>
2023-02-24 15:06:35 +0100

    color changing added functions

--- Exercise10,11,12/rectangle/src/components/rectangle.jsx
+++ Exercise10,11,12/rectangle/src/components/rectangle.jsx
@@ -1,32 +1,58 @@
 import React from 'react';
+import { useEffect } from 'react';
+import { useState } from 'react';
 
-let initialColour = {
-    backgroundColor: 'black',
-    width: 255,
-    height: 255,
-    color: 'white',
-    margin: 50
-};
+
 
 
 
 const Rectangle = () => {
+  
+    const [Rs, setRs] = useState(0);
+    const [Gs, setGs] = useState(0);
+    const [Bs, setBs] = useState(0);
 
+    useEffect(() => {
+        console.log('Rs or Gs or Bs state modified')
+        return () => {
+            console.log('Rectangle component is going to unmount...')
+        };
+    }, [Rs,Gs,Bs]);
+
+    let initialColour = {
+        backgroundColor: `rgb(${Rs},${Gs},${Bs})`,
+        width: 255,
+        height: 255,
+        color: 'white',
+        margin: 50
+    };
+
+    const intervalIdSetColour = 0;
+    
     const changeColourRandom = () => {
-            console.log("Mouse enter rectangle")     
-    }
+    console.log("Mouse enter rectangle and inside");
+    const tempRs = Math.floor(Math.random()* 256);
+    setRs(tempRs);
+    const tempGs = Math.floor(Math.random()* 256);
+    setRs(tempGs);
+    const tempBs = Math.floor(Math.random()* 256);
+    setRs(tempBs);
+    };
 
-    const stopColorLeave = () => {
+    const intervalColour = () => {
+        intervalIdSetColour = setInterval(changeColourRandom, 1000)
+    };
+   
+
+    const stopColour = () => {
         console.log('Mouse leave rectangle')
-    }
-
-    const stopColorClick = () => {
-        console.log('Mouse click 2 times')
+        // TODO: not working, see how to clear interval 
+        clearInterval(intervalIdSetColour)
     }
 
     return (
         <div>
-            <div onMouseEnter={changeColourRandom} onMouseLeave={stopColorLeave} onDoubleClick={stopColorClick} style={ initialColour }></div>
+            <div onMouseEnter={intervalColour} onMouseLeave={stopColour} onDoubleClick={stopColour} style={ initialColour }></div>
         </div>
     );
 };
