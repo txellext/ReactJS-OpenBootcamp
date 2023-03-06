364f8b3fd4028acf0ab5ce747301a93d0f95c5cc
Txell Amigó <txellmgsol@gmail.com>
2023-03-06 10:21:04 +0100

    yup added and working part1

--- firstProject/hola-mundo/src/components/pure/forms/loginFormik.jsx
+++ firstProject/hola-mundo/src/components/pure/forms/loginFormik.jsx
@@ -2,11 +2,23 @@ import React from 'react';
 import { Formik, Field, Form } from 'formik';
 import * as Yup from 'yup';
 
+const loginSchema = Yup.object().shape(
+    {
+        email: Yup.string()
+            .email('InvalidEmail format')
+            .required('Email is required'),
+        password: Yup.string()
+            .required('Password i s required')    
+    }
+);
+
+
+
 const LoginFormik = () => {
 
     const initialCredentials = {
-        email: ' ',
-        password: ' ',
+        email: '',
+        password: '',
     }
 
 
@@ -15,25 +27,47 @@ const LoginFormik = () => {
         <div>
             <h1>Login Formik</h1>
             <Formik
-                initialValues={ initialCredentials }                
+                // *** Initial values that form will take ***
+                initialValues={ initialCredentials }  
+                // *** Yup Validation Schema ***
+                validationSchema = { loginSchema }  
+                // *** onSubmit Event ***         
                 onSubmit={async (values) => {
                     await new Promise((r) => setTimeout(r, 500));
                     alert(JSON.stringify(values, null, 2));
                 }}               
             >
-                <Form>
-                    <label htmlFor="email">Email</label>
-                    <Field id="email" type="email" name="email" placeholder="example@email.com" />
 
-                    <label htmlFor="password">Password</label>
-                    <Field
-                        id="password"
-                        name="password"
-                        placeholder="password"
-                        type="password"
-                    />
-                    <button type="submit">Login</button>
-                </Form>
+                {/** We obtain props from Formik */}
+                
+                { props => {
+                    const {
+                        values,
+                        touched,
+                        errors,
+                        isSubmitting,
+                        handleChange,
+                        handleBlur,
+                    } = props;
+
+                    return (
+                        <Form>
+                            <label htmlFor="email">Email</label>
+                            <Field id="email" type="email" name="email" placeholder="example@email.com" />
+
+                            <label htmlFor="password">Password</label>
+                            <Field
+                                id="password"
+                                name="password"
+                                placeholder="password"
+                                type="password"
+                            />
+                            <button type="submit">Login</button>
+                        </Form>
+                   )
+                }}
+
+
             </Formik>
         </div>
     );
