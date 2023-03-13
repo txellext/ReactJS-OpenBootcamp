0f5a662f0ebb6729275572b160706533645f920d
Txell Amigó <txellmgsol@gmail.com>
2023-03-13 17:06:28 +0100

    link added to render

--- firstProject/hola-mundo/src/AppRoutingOne.js
+++ firstProject/hola-mundo/src/AppRoutingOne.js
@@ -1,18 +1,31 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import HomePage from './pages/home/HomePage';
 import NotFoundPage from './pages/404/NotFoundPage';
+import AboutPage from './pages/about-faqs/AboutPage';
 
 function AppRoutingOne() {
   return (
     <Router>
-    {/**Espera un conjunto de rutas */}
-      <Routes> 
-        {/**Cargamos una ruta exacta de la raíz del componente HomePage */}
-        <Route path='/' element={ <HomePage/> } />
-        {/*404 - Page Not Found */}
-        <Route path='*' element= { <NotFoundPage/> } />
+      <div>
+        <aside>
+          <Link to='/' >|| HOME | </Link>
+          <Link to='/about' >| ABOUT | </Link>
+          <Link to='/faqs' >| FAQs || </Link>
+          <Link to='/404' >| Not Existing Route || </Link>
+        </aside>
 
-      </Routes>
+        <main>
+          {/**Espera un conjunto de rutas */}
+          <Routes> 
+            {/**Cargamos una ruta exacta de la raíz del componente HomePage */}
+            <Route path='/' element={ <HomePage/> } />
+            {/*404 - Page Not Found */}
+            <Route path='*' element= { <NotFoundPage/> } />
+            <Route path='about' element={ <AboutPage />} />
+            <Route path="faqs" element={ <AboutPage />} />
+          </Routes>
+        </main>
+      </div>
     </Router>  
   );
 }
---
+++ firstProject/hola-mundo/src/pages/about-faqs/AboutPage.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const AboutPage = () => {
+    return (
+        <div>
+            <h1>
+                About | FAQs
+            </h1>
+
+            <div>
+                <button>
+                    
+                </button>
+            </div>
+        </div>
+    );
+}
+
+export default AboutPage;
